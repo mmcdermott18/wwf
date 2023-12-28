5a5de61645d6066d02f5c6b64a5abca896bfee31
mmcdermott18 <mmcdermott18@users.noreply.github.com>
2023-12-27 21:32:09 -0500

    thanks

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     // run function on initial page load
     slider();
+    form();
     // run function on resize of the window
     $(window).resize(function() {
       // slider();
@@ -17,23 +18,8 @@ function slider() {
     arrows: false,
     infinite: true,
     speed: 300,
-    // slidesToShow: 5,
     slidesToScroll: 1,
     centerMode: true,
     variableWidth: true,
-    // responsive: [
-    // {
-    //   breakpoint: 1300,
-    //   settings: {
-    //     slidesToShow: 3
-    //   }
-    // },
-    // { 
-    //   breakpoint: 1000,
-    //   settings: {
-    //     slidesToShow: 2
-    //   }
-    // }
-    // ]
   });
 }
